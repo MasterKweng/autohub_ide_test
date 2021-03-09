58fd2a2577c723286b715964b661e5b69bc08d58
yukwengru <masterkweng@163.com>
2021-03-09 18:40:51 +0800

    一些尝试

--- wxpython_demo/helloworld1.py
+++ wxpython_demo/helloworld1.py
@@ -1,4 +1,5 @@
 import wx
+from wx import Choicebook, Listbook, Notebook, Treebook, Toolbook
 
 class HelloFrame(wx.Frame):
 
@@ -26,21 +27,37 @@ class HelloFrame(wx.Frame):
 
         fileMenu = wx.Menu()
         helloItem = fileMenu.Append(-1, "&Hello...\tCtrl-H", "Help string shown in status bar for this menu item")
+
         fileMenu.AppendSeparator()
         exitItem = fileMenu.Append(wx.ID_EXIT)
 
         helpMenu = wx.Menu()
         aboutItem = helpMenu.Append(wx.ID_ABOUT)
 
+        bookMenu = wx.Menu()
+        bookItem = bookMenu.Append(wx.ID_ANY)
+
         menuBar = wx.MenuBar()
         menuBar.Append(fileMenu, "&File")
         menuBar.Append(helpMenu, "&Help")
+        menuBar.Append(bookMenu, "book")
 
         self.SetMenuBar(menuBar)
 
         self.Bind(wx.EVT_MENU, self.OnHello, helloItem)
         self.Bind(wx.EVT_MENU, self.OnExit, exitItem)
         self.Bind(wx.EVT_MENU, self.OnAbout, aboutItem)
+        self.Bind(wx.EVT_MENU, self.Oncbook, bookItem)
+
+    def bookHello(self):
+
+        cbook = Choicebook()
+        lbook = Listbook()
+        nbook = Notebook()
+        tbook = Toolbook()
+        trbook = Treebook()
+
+        self.Bind(cbook, self.oncbook, )
 
     def OnExit(self, event):
 
@@ -53,6 +70,10 @@ class HelloFrame(wx.Frame):
     def OnAbout(self, event):
 
         wx.MessageBox("sanple", "about", wx.OK|wx.ICON_INFORMATION)
+
+    def Oncbook(self, event):
+
+        wx.Choicebook()
     
 
 if __name__ == '__main__':
